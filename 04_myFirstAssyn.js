ba8f4f6fc03c983877a101e8b495d11337a3d783
Sandrine Gautier <sgautier75017@gmail.com>
2018-07-21 17:20:50 +0200

    correction ligne en double

--- 04_myFirstAssyn.js
+++ 04_myFirstAssyn.js
@@ -10,13 +10,6 @@ fs.readFile(file, callback);
 
 //on peut écrire la fonction callback soit dedans, soit en dehors. dans ce cas c'est en dehors. On l'appelle donc dans la ligne 10
 
-
-function callback(err, data) {
-    lines = data.toString().split('\n').length - 1;
-    console.log(lines);
-} // la fonction est de la ligne 5 à 9
-
-
 /*
 FONCTION CALLBACK DEDANS...
 fs.readFile(file, function callback(err, data) {
